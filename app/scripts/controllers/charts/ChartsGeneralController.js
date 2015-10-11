25f1791bb479e4d921ebe038d05adff87bf4d830
pamigomp <201011@student.pwr.wroc.pl>
2015-10-11 19:48:52 +0200

    Added mocked charts,

--- app/scripts/controllers/charts/ChartsGeneralController.js
+++ app/scripts/controllers/charts/ChartsGeneralController.js
@@ -3,13 +3,32 @@
 angular.module('RehApp')
 
         .controller('ChartsGeneralController', function ($scope) {
-            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-            $scope.series = ['Series A', 'Series B'];
+            $scope.labels = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+            $scope.series = ['Series A'];
             $scope.data = [
-                [65, 59, 80, 81, 56, 55, 40],
-                [28, 48, 40, 19, 86, 27, 90]
+                [6500, 5900, 8000, 8100, 5600, 5500, 4000, 4000, 1000, 6000, 4000, 2000]
             ];
-            $scope.onClick = function (points, evt) {
-                console.log(points, evt);
-            };
+
+            $scope.labels2 = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+            $scope.series2 = ['mgr Paweł Pietrzak', 'Jan Kowalski', 'Andrzej Nowak'];
+            $scope.data2 = [
+                [650, 590, 800, 810, 560, 550, 400, 400, 100, 600, 400, 200],
+                [100, 200, 500, 300, 450, 200, 150, 400, 600, 100, 300, 300],
+                [300, 500, 450, 300, 350, 300, 400, 350, 500, 200, 350, 400]
+            ];
+
+            $scope.labels3 = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+            $scope.series3 = ['KinesioTaping', 'Krioterapia', "Ultradźwięki"];
+            $scope.data3 = [
+                [300, 200, 500, 400, 560, 550, 400, 400, 300, 200, 300, 400],
+                [300, 500, 300, 300, 400, 300, 400, 550, 100, 200, 350, 400],
+                [100, 200, 600, 300, 450, 300, 150, 400, 800, 500, 300, 300]
+
+            ];
+
+            $scope.labels4 = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+            $scope.series4 = ['Series A'];
+            $scope.data4 = [
+                [350, 500, 600, 400, 700, 600, 550, 400, 475, 400, 600, 550]
+            ];
         });
